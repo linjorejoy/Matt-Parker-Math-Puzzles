1ecacd1abc1f213d9dfaeb77a88b08310a4ca6c8
AmazonianPanda <howyoudoing1618@gmail.com>
2020-11-04 17:57:05 +0530

    file added

--- SPOOKY MATHS/vampire_matrices.py
+++ SPOOKY MATHS/vampire_matrices.py
@@ -14,15 +14,20 @@ def getDivisors(number):
 
 
 def getVampireMatrices(order):
-    for a in range(10**order):
+    for a in range(10**(order-1), 10**order):
         d = 10**order + 1 - a
         if d < 10**order:
             bc = (10**order + 1)*a - a*a
             for b in getDivisors(bc):
-                if b < 10**order and bc/ b < 10**order:
-                    print("| {:^3} {:^3} || {:^3} {:^3} |".format(int(a), int(b), int(bc/b), int(d)))
-                    # print(" ")
-                    pass
+                if b < 10**order and bc/ b < 10**order and 10**(order-1) < d < 10**(order) :
+                    yield "|  {:^8} {:^8}  |\n|_ {:^8} {:^8} _|\n\n".format(int(a), int(b), int(bc/b), int(d))
 
 
-getVampireMatrices(2)
+def writeToFile(fileName, order):
+    gen = getVampireMatrices(order)
+    with open(fileName, 'w') as file:
+        for line in gen:
+            file.write(str(line))
+
+
+writeToFile("SPOOKY MATHS/vampire_matrices_1stOrder.txt", 1)
